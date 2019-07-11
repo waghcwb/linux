49aa475dadacc61c246cf9281c674f79409af97f
Wagner Souza <wagh.cwb@gmail.com>
2019-07-11 16:54:49 -0300

    core.autocrlf

--- git/config_git.sh
+++ git/config_git.sh
@@ -4,6 +4,7 @@ wget https://cdn.rawgit.com/waghcwb/linux/master/git/.gitignore --directory-pref
 
 git config --global core.excludesfile ~/.gitignore
 git config --global core.filemode false
+git config --global core.autocrlf true
 git config --global core.editor vim
 git config --global alias.lg "log --graph --abbrev-commit --decorate --date=iso --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%cd)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'"
 git config --global alias.st "status -sb"
