7e73701d3691126a0a01eaebe329987b2eb29503
Wagner Souza <wagh.cwb@gmail.com>
2019-08-08 23:13:44 -0300

    Update config_git.sh

--- git/config_git.sh
+++ git/config_git.sh
@@ -12,3 +12,4 @@ git config --global merge.ours.driver true
 git config --global pull.ff only
 git config --global merge.ff false
 git config --global alias.conflicts "diff --name-only --diff-filter=U"
+git config --global credential.helper store
